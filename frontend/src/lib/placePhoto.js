312312c57dc6ac1b22c5aff1d2af01a887fe2172
Mamimumemo1202 <m.shinnosuke1202@gmail.com>
2025-09-30 21:40:18 +0900

    写真がない場合はnull

--- frontend/src/lib/placePhoto.js
+++ frontend/src/lib/placePhoto.js
@@ -1,5 +1,5 @@
 export function getPlacePhotoUrl(photoRef) {
-  if (!photoRef) return console.log("No image");
+  if (!photoRef) return null
 
   return `http://localhost:3000/api/v1/photos?photo_ref=${encodeURIComponent(photoRef)}`;
 }
