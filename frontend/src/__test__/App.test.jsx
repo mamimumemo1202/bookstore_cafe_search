5e136f32f3cc34e28bc4e70ae584f5c40be95dae
Mamimumemo1202 <m.shinnosuke1202@gmail.com>
2025-10-14 20:19:36 +0900

    Testのセットアップ

---
+++ frontend/src/__test__/App.test.jsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import { describe, test } from "vitest";
+import {App} from "../App.jsx";
+
+describe("App", () => {
+  test("renders App component", () => {
+    render(<App/>);
+
+    screen.debug();
+  });
+});
+
