e645a01746b2095d6de205aa6d5db3a655222c68
Mamimumemo1202 <m.shinnosuke1202@gmail.com>
2025-10-22 00:35:56 +0900

    corsエラーの調査

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -6,13 +6,20 @@
 # Read more: https://github.com/cyu/rack-cors
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  # allow do
+
+  #   origins "https://bookstore-cafe-search.pages.dev/", "http://localhost:5173" 
+
+  #   resource "/api/v1/*",
+  #     headers: :any,
+  #      expose: [ "access-token", "client", "uid" ],
+  #     methods: [ :get, :post, :put, :patch, :delete, :options, :head ]
+  # end
+
   allow do
-
-    origins "https://bookstore-cafe-search.pages.dev/", "http://localhost:5173" 
-
-    resource "/api/v1/*",
-      headers: :any,
-       expose: [ "access-token", "client", "uid" ],
-      methods: [ :get, :post, :put, :patch, :delete, :options, :head ]
+    origins "*"
+    resource "/api/v1/*", headers: :any, methods: [:get, :options, :head]
   end
+
 end
+  
