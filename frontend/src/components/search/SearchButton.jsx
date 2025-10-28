7deaacb079cb46bc68c5aaf9e5412a60bf106fb3
Mamimumemo1202 <m.shinnosuke1202@gmail.com>
2025-10-28 21:33:23 +0900

    useLocationSearchとSearchButtonの統合

--- frontend/src/components/search/SearchButton.jsx
+++ frontend/src/components/search/SearchButton.jsx
@@ -1,16 +1,38 @@
-import { useLocationSearch } from '../../hooks/useLocationSearch';
+import { useGeolocation } from '../../hooks/useGeolocation';
+import { useNavigate } from 'react-router-dom';
+import { useModal } from '../contexts/ModalContext';
+import { useLoading } from '../contexts/LoadingContext';
+import { toast } from 'react-toastify';
+
 
 export function SearchButton({ label, searchMode }) {
-  const { handleSearch } = useLocationSearch();
+  const { getLocation } = useGeolocation();
+  const navigate = useNavigate();
+  const { closeModal } = useModal();
+  const { withLoading } = useLoading();
+
+  const handleSearch = async () => {
+    await withLoading(async () => {
+      try {
+        const pos = await getLocation();
+        navigate(`/search?lat=${pos.lat}&lng=${pos.lng}&mode=${searchMode}`);
+        closeModal();
+      } catch (error) {
+        toast.error('位置情報を取得できませんでした');
+        closeModal();
+        navigate('/', { replace: true });
+      }
+    });
+  };
 
   return (
     <>
-      <div
+      <button
         className="flex text-gray-700 rounded-md border border-gray-700 px-4 py-2 m-1 text-starts cursor-pointer hover:bg-gray-700 hover:text-white"
-        onClick={() => handleSearch(searchMode)}
+        onClick={() => handleSearch()}
       >
         {label}
-      </div>
+      </button>
     </>
   );
 }
