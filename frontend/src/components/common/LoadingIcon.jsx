3e6c0c0b61f138f591faf2585065d88380e481e6
Mamimumemo1202 <m.shinnosuke1202@gmail.com>
2025-10-28 21:37:47 +0900

    Loadingコンテキスト化による不必要なファイルの削除

--- frontend/src/components/common/LoadingIcon.jsx
+++
@@ -1,9 +0,0 @@
-import { StarIcon } from '@heroicons/react/24/outline';
-
-export function LoadingIcon() {
-  return (
-    <>
-      <StarIcon className="animate-bounce w-6 h-6 text-gray-700" />
-    </>
-  );
-}
