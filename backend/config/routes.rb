a814498c5195e6d0ddee1608c9b90ddaaa7586c6
Mamimumemo1202 <m.shinnosuke1202@gmail.com>
2025-11-07 00:57:59 +0900

    別オリジンの炉倒れ句とを許可する

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       mount_devise_token_auth_for "User", at: "auth",
-      controllers: { registrations: "api/v1/overrides/registrations" }
+      controllers: { 
+        registrations: "api/v1/overrides/registrations",
+        confirmations: "api/v1/overrides/confirmations" }
 
       resources :places, only: [ :index, :create, :show ] do
         collection do
