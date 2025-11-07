0cffa5c0d40848e7cb82af8992c1f96f7a6ea4af
Mamimumemo1202 <m.shinnosuke1202@gmail.com>
2025-11-07 10:33:22 +0900

    でバック

--- backend/app/controllers/api/v1/overrides/confirmations_controller.rb
+++ backend/app/controllers/api/v1/overrides/confirmations_controller.rb
@@ -3,8 +3,10 @@ class Api::V1::Overrides::ConfirmationsController < DeviseTokenAuth::Confirmatio
     ALLOWED_REDIRECT_HOSTS = [(URI.parse(ENV["FRONTEND_BASE_URL"]).host rescue nil)].compact.freeze
 
     def show
+        
     super do
       uri = URI.parse(@redirect_url) rescue nil
+      Rails.logger.info("[CONFIRM] redirect_url=#{@redirect_url} env=#{ENV['FRONTEND_BASE_URL']}")
       if uri && ALLOWED_REDIRECT_HOSTS.include?(uri.host)
         return redirect_to(@redirect_url, allow_other_host: true)
       end
